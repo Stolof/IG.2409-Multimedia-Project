be13016ee2ad3f6810b69f193594b002594fda3b
leonardoboldrini <leonardo.boldrini95@gmail.com>
2018-05-27 18:51:21 +0200

    changes applied to main.cpp to better understand called functions. 2.4 still missing

--- program/main.cpp
+++ program/main.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 using namespace cv;
 
-#include "bright.h"
-#include "resize.h"
+
 #include "dil.h"
+#include "resize.h"
+#include "bright.h"
+
 #include "cannyEdge.h"
 
 
@@ -17,12 +19,24 @@ int main( int argc, char** argv )
     }
 	string srcName = argv[2];
 	string dstName = argv[3];
-    if (strcmp(argv[1],"bright")==0){
-    	double alpha = atof(argv[4]);
-	double beta = atof(argv[5]);
-	int bright = modifyBrightness(srcName,dstName,alpha,beta);
-	if (bright ==1){
-            cout<<"Brightness changed!"<<endl;	
+
+    if (strcmp(argv[1],"dilatationerosion")==0){
+	
+	int choiseParam;
+	int sizeParam;
+	cout << "Enter 1 for dilatation and 0 for erosion" << endl;
+	cin >> choiseParam;
+	sizewrong: cout << "Choose the number of iterartions between 1-10" << endl;
+	cin >> sizeParam;
+	if (sizeParam < 1 || sizeParam > 10)
+	{
+	cout << "The number of iterations has to be between 1 and 10" << endl;
+	goto sizewrong;
+	}
+	
+	int dilatation=dil(srcName, dstName,choiseParam, sizeParam);
+	if (dilatation==1){
+            cout<<"changes applied"<<endl;	
 	}else{
 	    cout<<"An error occured"<<endl;
 	}
@@ -47,6 +61,7 @@ int main( int argc, char** argv )
 	cout << "The scaling factor for the Y axis has to be between 0.1 and 3" << endl;
 	goto ywrong;
 	}
+
 	int res=resize(srcName, dstName, scaleX, scaleY);
 	if (res ==1){
             cout<<"resized!"<<endl;	
@@ -55,38 +70,51 @@ int main( int argc, char** argv )
 	}
     }
 
-    if (strcmp(argv[1],"dilatationerosion")==0){
+    if (strcmp(argv[1],"bright")==0){
+    	double alpha;  /* atof(argv[4]); */
+	double beta;
 	
-	int choiseParam;
-	int sizeParam;
-	cout << "Enter 1 for dilation and 0 for erosion" << endl;
-	cin >> choiseParam;
-	sizewrong: cout << "Choose the number of iterartions between 1-10" << endl;
-	cin >> sizeParam;
-	if (sizeParam < 1 || sizeParam > 10)
+	scale: cout << "Choose a scale factor between 0.1 and 5" << endl;
+	cin >> alpha;
+	if (alpha < 0.1 || alpha >5)
 	{
-	cout << "The number of iterations has to be between 1 and 10" << endl;
-	goto sizewrong;
+	cout << "The scale factor has to be between 0.1 and 5" << endl;
+	goto scale;
 	}
-	
-	int dilatation=dil(srcName, dstName,choiseParam, sizeParam);
-	if (dilatation==1){
-            cout<<"changes applied"<<endl;	
+
+	shift: cout << "Choose a shift factor between -100 and 100" << endl;
+	cin >> beta;
+	if (beta < -100 || beta >100)
+	{
+	cout << "The shift factor has to be between -100 and 100" << endl;
+	goto shift;
+	}
+
+	int bright = modifyBrightness(srcName,dstName,alpha,beta);
+	if (bright ==1){
+            cout<<"Brightness changed!"<<endl;	
 	}else{
 	    cout<<"An error occured"<<endl;
 	}
     }
 
+    if (strcmp(argv[1],"panoramastitch")==0)
+    {
+	
+    }
 
     if (strcmp(argv[1],"cannyEdge")==0)
     {
 	int thr1, thr2, ker;
-	cout << "Enter the low threshold" /*values?*/ << endl;
+	th1: cout << "Enter the low threshold between 50 and 150" << endl;
 	cin >> thr1;
-	cout << "Enter the max threshold" /*values?*/ << endl;
+	if (thr1 < 50 || thr1 >150){ cout << "Please choose a threshold between 50 and 150" << endl; goto th1;}
+	th2: cout << "Enter the max threshold between 150 and 450" << endl; /*because it's recommended to use max=3*low thresholds*/
 	cin >> thr2;
-	cout << "Enter the size of the kernel" /*values?*/ << endl;
+	if (thr2 < 150 || thr2 >450){ cout << "Please choose a threshold between 150 and 450" << endl; goto th2;}
+	k: cout << "Enter the size of the kernel between 3, 5 and 7" << endl;
 	cin >> ker;
+	if (ker != 3 && ker != 5 && ker != 7){ cout << "Please choose a size between 3, 5 and 7" << endl; goto k;}
 	int canEdge = cannyEdge(srcName, dstName, thr1, thr2, ker);
 	if (canEdge ==1)
         {
